59ea9ab34178d3799a92bf55af9f0257bef4c1d0
Altarick <aaaa@bbb.ccc>
2021-02-10 19:52:17 +0100

    fixed anchor

--- Flying Knights/Assets/Scripts/PlayerJoint.cs
+++ Flying Knights/Assets/Scripts/PlayerJoint.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class PlayerJoint : MonoBehaviour
 {
+    [SerializeField] float limitDistance = 15;
     private ConfigurableJoint hookJoint = null;
+    bool hooked = false;
+
+    private float calculateDistance()
+    {
+        return (hookJoint.connectedBody.transform.position - transform.position).magnitude;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+    }
+
+    void FixedUpdate()
+    {
+        if(hooked && calculateDistance() > limitDistance + 0.001 )
+        {
+            //setting correct position
+            transform.position = hookJoint.connectedBody.transform.position + (transform.position - hookJoint.connectedBody.transform.position).normalized*limitDistance;
+            //straigthening the speed
+            GetComponent<Rigidbody>().velocity = Vector3.ProjectOnPlane(GetComponent<Rigidbody>().velocity,(transform.position - hookJoint.connectedBody.transform.position).normalized) * GetComponent<Rigidbody>().velocity.magnitude;
+        }
     }
 
     public void hook(Rigidbody toConnect,float distance,Vector3 localAnchor)
@@ -19,13 +36,13 @@ public class PlayerJoint : MonoBehaviour
         hookJoint.xMotion = ConfigurableJointMotion.Limited;
         hookJoint.yMotion = ConfigurableJointMotion.Locked;
         hookJoint.zMotion = ConfigurableJointMotion.Locked;
-        hookJoint.anchor = transform.InverseTransformPoint(toConnect.transform.position);
-        hookJoint.autoConfigureConnectedAnchor = true;
-        //hookJoint.connectedAnchor = transform.InverseTransformPoint(localAnchor);
+        hookJoint.anchor = transform.InverseTransformPoint(localAnchor); 
+        hookJoint.autoConfigureConnectedAnchor = false;
+        hookJoint.connectedAnchor = transform.InverseTransformPoint(toConnect.transform.position);
         
         SoftJointLimit limits = new SoftJointLimit();
         limits.limit = distance;
-        limits.contactDistance = 5;
+        limits.contactDistance = 4;
         hookJoint.linearLimit = limits;
     }
 
