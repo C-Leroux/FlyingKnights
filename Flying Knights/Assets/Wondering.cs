da5f5e3ed853ac639a46f720564a37b6dc41c5c2
Dorian DOAN <dorian.doan@gmail.com>
2021-02-18 20:44:10 +0100

    Wondering

---
+++ Flying Knights/Assets/Wondering.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Wondering : MonoBehaviour
+{
+    float walkSpeed = 5f;
+    bool isWondering = false;
+    public NavMeshAgent navmeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isWondering)
+        {
+            StartCoroutine(wonder());
+        }
+    }
+
+    IEnumerator wonder()
+    {
+        isWondering = true;
+        float walkingTime = Random.Range(0, 5);
+        navmeshAgent.SetDestination(RandomNavmeshLocation(walkingTime * walkSpeed));
+        yield return new WaitForSeconds(walkingTime);
+        isWondering = false;
+    }
+
+    Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
+    }
+}
