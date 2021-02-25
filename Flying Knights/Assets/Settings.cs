c61d48944da1f64a53f3e53bbf0dd7884ab52d1b
Tristan_poly <trsitan.damian@etu.univ-lyon1.fr>
2021-02-25 00:14:44 -0500

    Settings Menu
    Volume Slider
    Graphic Quality choice

--- Flying Knights/Assets/Settings.cs
+++ Flying Knights/Assets/Settings.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Settings : MonoBehaviour
 {
     public GameObject Menu;
     public GameObject SettingMenu;
-    void Start()
+    public AudioMixer AudioMixer;
+
+    public void SetVolume(float Volume)
     {
-
-
+        AudioMixer.SetFloat("volume", Volume*100-80);
     }
 
+    public void setQuality (int quality)
+    {
+        Debug.Log(quality);
+        QualitySettings.SetQualityLevel(quality);
+    }
     public void Back()
     {
         SettingMenu.SetActive(false);
         Menu.SetActive(true);
+
     }
 }
