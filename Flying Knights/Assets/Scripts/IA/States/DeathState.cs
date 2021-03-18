bd6ea20457d5b14a2e7a63c3247b328e8b28920b
Corentin LEROUX <corentin.leroux@efrei.net>
2021-03-18 04:30:55 +0100

    added a death state to the colossus

---
+++ Flying Knights/Assets/Scripts/IA/States/DeathState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathState : MonoBehaviour
+{
+    private static DeathState instance = null;
+
+    public static DeathState Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new DeathState();
+            }
+            return instance;
+        }
+    }
+
+    public void Enter(Colossus colossus)
+    {
+        // Start Dying
+        colossus.Die();
+    }
+
+    public void Execute(Colossus colossus)
+    {
+
+    }
+
+    public void Exit(Colossus colossus)
+    {
+
+    }
+
+    // public bool OnMessage(Colossus colossus, const Telegram& msg);
+}
