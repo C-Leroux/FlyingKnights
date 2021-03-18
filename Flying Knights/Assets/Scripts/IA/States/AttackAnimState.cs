319228057dd358cf7c58e88d81787b74baa758e7
Corentin LEROUX <corentin.leroux@efrei.net>
2021-03-18 12:35:31 +0100

    state attackanim added

---
+++ Flying Knights/Assets/Scripts/IA/States/AttackAnimState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//------------------------------------------------------------------------
+//  In this state, the colossus do an attack animation, and thus can't
+//  move until it's done.
+//------------------------------------------------------------------------
+public class AttackAnimState : State<Colossus>
+{
+    private static AttackAnimState instance = null;
+
+    public static AttackAnimState Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new AttackAnimState();
+            }
+            return instance;
+        }
+    }
+
+    public void Enter(Colossus colossus)
+    {
+        colossus.AttackAnim();
+    }
+
+    public void Execute(Colossus colossus)
+    {
+        if (colossus.IsRecovered())
+            colossus.FSM.ChangeState(AttackState.Instance);
+    }
+
+    public void Exit(Colossus colossus)
+    {
+
+    }
+
+    // public bool OnMessage(Colossus colossus, const Telegram& msg);
+}
