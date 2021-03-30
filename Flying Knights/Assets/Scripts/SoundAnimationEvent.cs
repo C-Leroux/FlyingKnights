2804258d13ca1d2aeb99d904d52f9297fb2ac0ff
Dorian DOAN <dorian.doan@gmail.com>
2021-03-30 19:24:11 +0200

    Bruitage pas joueur + bruitage mort colosse

--- Flying Knights/Assets/Scripts/SoundAnimationEvent.cs
+++ Flying Knights/Assets/Scripts/SoundAnimationEvent.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class SoundAnimationEvent : MonoBehaviour
 {
-    [SerializeField] AudioSource audio;
+    AudioSource audio;
+    [SerializeField] AudioSource[] Sources;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +18,31 @@ public class SoundAnimationEvent : MonoBehaviour
         
     }
 
-    public void PlaySource()
+    //Sources du player possede 2 sources : 0 - Slash d'epee
+    //                                      1 - Bruit de pas  
+    public void PlayStepPlayer()
     {
+        audio = Sources[1];
         audio.Play();
     }
+    public void PlaySlash()
+    {
+        audio = Sources[0];
+        audio.Play();
+    }
+
+    //Sources du colosse possede 2 source : 0 - Bruit de pas
+    //                                      1 - Cri de mort
+    public void PlayStepColosse()
+    {
+        audio = Sources[0];
+        audio.Play();
+    }
+
+    public void PlayDieColosse()
+    {
+        audio = Sources[1];
+        audio.Play();
+    }
+
 }
