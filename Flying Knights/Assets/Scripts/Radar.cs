68d61633fd47fe4bc9e49d9e3f61a5c8b5850619
Corentin LEROUX <corentin.leroux@efrei.net>
2021-04-08 00:31:44 +0200

    radar added to one scene

---
+++ Flying Knights/Assets/Scripts/Radar.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    private GameObject nearestColossus;
+    private float distToNearest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindNearestColossus();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindNearestColossus();
+    }
+
+    private void FindNearestColossus()
+    {
+        GameObject[] colossusList = GameObject.FindGameObjectsWithTag("Colossus");
+        GameObject curNearest = null;
+        float curDist = Mathf.Infinity;
+        foreach (GameObject colossus in colossusList)
+        {
+            if (colossus.GetComponent<Colossus>().IsDead())
+                continue;
+            float dist = Vector3.Distance(colossus.transform.position, player.transform.position);
+            if (dist < curDist)
+            {
+                curDist = dist;
+                curNearest = colossus;
+            }
+        }
+        nearestColossus = curNearest;
+        distToNearest = curDist;
+    }
+
+    public Vector3 GetDirVector()
+    {
+        Vector3 colossusPos = nearestColossus.transform.position;
+        Vector3 playerPos = player.transform.position;
+        colossusPos.y = 0;
+        playerPos.y = 0;
+        return (colossusPos - playerPos).normalized;
+    }
+
+    public Vector3 GetColossusPos()
+    {
+        return nearestColossus.transform.position + new Vector3(0, nearestColossus.transform.localScale.y * 2, 0);
+    }
+
+    public float GetDist()
+    {
+        return distToNearest;
+    }
+
+    public bool IsColossus()
+    {
+        return nearestColossus != null;
+    }
+}
---
+++ Flying Knights/Assets/Scripts/RadarPrint.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadarPrint : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private Radar radar;
+    [SerializeField]
+    private RectTransform arrowRect;
+    [SerializeField]
+    private RectTransform canvasRect;
+    [SerializeField]
+    private Image radarImg;
+
+    private Vector3 dirArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!radar.IsColossus())
+        {
+            SetVisible(false);
+            return;
+        }
+
+        //Vector3 dir = radar.GetDirVector();
+        //float angle = Vector3.Angle(dir, cam.transform.forward);
+        //arrowRect.localEulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 screenPoint = GetColossusScreenPoint();
+        //Vector3 cappedScreenPoint = Capping(screenPoint);
+
+        Vector3 arrowPos;
+        float xlimit = canvasRect.sizeDelta.x / 2 - canvasRect.sizeDelta.x / 10;
+        float ylimit = canvasRect.sizeDelta.y / 2 - canvasRect.sizeDelta.y / 10;
+
+        if (screenPoint.x < -xlimit && screenPoint.y > 0 || screenPoint.x > xlimit && screenPoint.y < 0)
+        {
+            arrowPos = new Vector3(-xlimit, 0, 0);
+            arrowRect.localEulerAngles = new Vector3(0, 0, 90);
+        }
+        else if (screenPoint.x > xlimit && screenPoint.y > 0 || screenPoint.x < -xlimit && screenPoint.y > 0)
+        {
+            arrowPos = new Vector3(xlimit, 0, 0);
+            arrowRect.localEulerAngles = new Vector3(0, 0, -90);
+        }
+        else
+        {
+            arrowRect.localEulerAngles = new Vector3(0, 0, 180);
+            if (screenPoint.z < 0)
+                arrowPos = new Vector3(0, -ylimit, 0);
+            else
+            {
+                if (radar.GetDist() < 50)
+                {
+                    SetVisible(false);
+                    return;
+                }
+                arrowPos = screenPoint;
+            }
+        }
+
+        SetVisible(true);
+        Vector3 arrowWorldPosition = cam.ScreenToWorldPoint(arrowPos);
+        arrowRect.localPosition = new Vector3(arrowPos.x, arrowPos.y, -1f);
+    }
+
+    private Vector3 GetColossusScreenPoint()
+    {
+        radar.GetColossusPos();
+
+        Vector3 viewportPos = cam.WorldToViewportPoint(radar.GetColossusPos());
+
+        Debug.Log(viewportPos);
+
+        return new Vector3(
+            ((viewportPos.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f)),
+            ((viewportPos.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f)),
+            viewportPos.z
+            );
+    }
+
+    private Vector3 Capping(Vector3 toCap)
+    {
+        Vector3 cappedScreenPoint = toCap;
+        float xlimit = canvasRect.sizeDelta.x / 2 - canvasRect.sizeDelta.x / 10;
+        float ylimit = canvasRect.sizeDelta.y / 2 - canvasRect.sizeDelta.y / 10;
+
+        if (cappedScreenPoint.x < -xlimit) cappedScreenPoint.x = -xlimit;
+        if (cappedScreenPoint.x > xlimit) cappedScreenPoint.x = xlimit;
+        if (cappedScreenPoint.y < -ylimit) cappedScreenPoint.y = -ylimit;
+        if (cappedScreenPoint.y > ylimit) cappedScreenPoint.y = ylimit;
+
+        if (cappedScreenPoint.z < 0 && Mathf.Abs(cappedScreenPoint.x) != xlimit && Mathf.Abs(cappedScreenPoint.y) != ylimit)
+        {
+            cappedScreenPoint.y = -ylimit;
+        }
+
+        return cappedScreenPoint;
+    }
+
+    private void SetVisible(bool b)
+    {
+        Color color = radarImg.color;
+        if (b)
+            color.a = 100;
+        else
+            color.a = 0;
+        radarImg.color = color;
+    }
+
+    private void UpdateDir()
+    {
+        dirArrow = radar.GetDirVector();
+    }
+
+    private void UpdateArrow()
+    {
+
+    }
+}
