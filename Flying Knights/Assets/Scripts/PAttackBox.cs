f2feda840af69e11af6924e62a86f4b6d8d863a9
Kuruya <rosaphira.the.reaper@gmail.com>
2021-04-07 16:02:28 +0200

    Last commit before pull

--- Flying Knights/Assets/Scripts/PAttackBox.cs
+++ Flying Knights/Assets/Scripts/PAttackBox.cs
@@ -7,10 +7,6 @@ public class PAttackBox : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "WeakPoint")
-        {
-            Debug.Log("I hit the Colossus weak point");
-            // Call a function in the colossus script to activate the damage.
-        }
+        
     }
 }
---
+++ Flying Knights/Assets/Scripts/SFXPlayerAttack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayerAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject woodSFX;
+    [SerializeField] private GameObject bloodSfx;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wood"))
+        {
+            StartCoroutine(UseSFX(woodSFX));
+        }
+        
+        else if (other.CompareTag("Colossus"))
+        {
+            StartCoroutine(UseSFX(bloodSfx));
+        }
+    }
+
+    private IEnumerator UseSFX(GameObject sfx)
+    {
+        sfx.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        sfx.SetActive(false);
+    }
+}
